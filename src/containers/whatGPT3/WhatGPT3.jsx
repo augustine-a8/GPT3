92ba9b82c104ec850b2ee72d70e041ff8f10dc39
augustine-a8 <logos.agbeko@gmail.com>
2022-01-16 19:46:00 +0000

    header and brand components built

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,5 +1,23 @@
+import { Feature } from '../../components';
 import './whatGPT3.css';
 
 export default function WhatGPT3() {
-  return <div>WhatGP3</div>;
+  return (
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient-text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore the library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
+    </div>
+  );
 }
